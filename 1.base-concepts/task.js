3c212d31b21c8f2b94901357342c73ebcbf3eabd
ratedev <ratedwounder@gmail.com>
2022-02-01 17:10:35 +0200

    fix mistakes

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,19 +1,18 @@
 function solveEquation(a, b, c) {
   let arr = [];
   let d = b ** 2 - 4 * a * c;
-  let x0 = (-b + Math.sqrt(d)) / (2 * a);
-  let x1 = (-b + Math.sqrt(d)) / (2 * a);
-  let x2 = (-b - Math.sqrt(d)) / (2 * a);
 
-  if (d < 0) {
-    arr = [];
-  } else if (d == 0) {
+   if (d == 0) {
+    let x0 = -b / (2 * a);
     arr.push(x0);
     
   } else if (d > 0) {
+    let x1 = (-b + Math.sqrt(d)) / (2 * a);
+    let x2 = (-b - Math.sqrt(d)) / (2 * a);
     arr.push(x1);
     arr.push(x2);
   }
+  
   return arr; 
 }
 
