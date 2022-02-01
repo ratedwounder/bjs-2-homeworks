31fa5d01320738db2064e2d75e7686ef2b9837a5
ratedev <ratedwounder@gmail.com>
2022-02-01 08:17:39 +0200

    complete 1st task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,20 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  let d = b ** 2 - 4 * a * c;
+  let x0 = (-b + Math.sqrt(d)) / (2 * a);
+  let x1 = (-b + Math.sqrt(d)) / (2 * a);
+  let x2 = (-b - Math.sqrt(d)) / (2 * a);
+
+  if (d < 0) {
+    arr = [];
+  } else if (d == 0) {
+    arr.push(x0);
+    
+  } else if (d > 0) {
+    arr.push(x1);
+    arr.push(x2);
+  }
+  return arr; 
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
