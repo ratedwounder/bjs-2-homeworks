5be5aecda581a569a26728abcfc70dd1fe55ef2e
ratedev <ratedwounder@gmail.com>
2022-02-06 22:07:25 +0200

    complete homework

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,71 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
+  let avg;
+  let counter = 0;
 
-  // Ваш код
+  for (let item of arr) {
+    if (item > max) {
+      max = item;
+    }
+    if (item < min) {
+      min = item;
+    }
+    sum += item;
+    counter++;
+      
+  }
 
-  return { min: min, max: max, avg: avg };
+  avg = Number((sum / counter).toFixed(2));
+
+  return {
+    min: min,
+    max: max,
+    avg: avg
+  };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
+  for (let item of arr) {
+    sum += item;
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = 0;
 
-  // Ваш кода
-  // for ...
-  
+  for (let item of arrOfArr) {
+    const worker = func(item);
+
+    if (worker > max) {
+      max = worker;
+    }
+  }
+
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min = arr[0];
+  let max = arr[0];
+  let diffMaxMin = 0;
+
+  for (let item of arr) {
+    if (item > max) {
+      max = item;
+    }
+    if (item < min) {
+      min = item;
+    }
+  }
+
+  return diffMaxMin = Math.abs(max - min);
 }
