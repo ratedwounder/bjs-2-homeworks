66f4ac780f7da24798c71fd71b0cd25ad0483c26
ratedev <ratedwounder@gmail.com>
2022-02-09 23:48:20 +0200

    homework done

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,9 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
+  let result = arr1.length == arr2.length && arr1.every((v, i) => arr2[i] == v);
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
+  let resultArr = arr.filter(item => item > 0).filter(item => item % 3 == 0).map(item => item * 10)
   return resultArr; // array
 }
