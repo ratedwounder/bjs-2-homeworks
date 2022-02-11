4a85a7910a4e0762eae92696dd6b83f39ee94ee6
ratedev <ratedwounder@gmail.com>
2022-02-11 23:46:36 +0200

    homework done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,42 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+    this.subjectName = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [];
+    this.marks.push(mark)
+  }
+   else {
+    this.marks.push(mark)
+  }
+}
+
+Student.prototype.addMarks = function (...marks) {
+  this.marks = [];
+   for (let mark of marks) {
+      this.marks.push(mark)
+   }
+}
+
+Student.prototype.getAverage = function (...marks) {
+  let counter = 0;
+  let sum = 0; 
+  for (let mark of marks) {
+    sum += mark;
+    counter++;
+  } 
+  return sum/counter
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subjectName;
+  delete this.marks;
+  this.excluded = reason;
+}
