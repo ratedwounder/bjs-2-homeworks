af3c885484ddb269a9cec30967345de64052ef21
ratedev <ratedwounder@gmail.com>
2022-03-07 12:26:13 +0200

    done homework

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,68 @@
+//task 1
+
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  function wrapper(...rest) {
+    let hash = rest.join(',');
+    let existResult = cache.filter(cacheRecord => cacheRecord.hash === hash);
+    if (existResult.length === 1) {
+      console.log('Из кэша: ' + existResult[0].value);
+      return 'Из кэша: ' + existResult[0].value;
+    } else {
+      let value = func.call(this, ...rest);
+      console.log('Вычисляем: ' + value);
+      if (cache.length < 5) {
+        cache.push({
+          hash,
+          value
+        });
+      } else {
+        cache.unshift({
+          hash,
+          value
+        });
+        cache.pop();
+      }
+      return 'Вычисляем: ' + value;
+    }
+  }
+  return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+// task 2
+
+function debounceDecoratorNew(func, ms) {
+
+  let timeout;
+  let repeatCall = false;
+
+  function wrapper(...rest) {
+
+    if (!repeatCall) {
+      func.apply(this, ...rest);
+      repeatCall = true;
+      return;
+    }
+    clearTimeout(timeout)
+    timeout = setTimeout(() => {
+      repeatCall = false
+      func.apply(this, ...rest)
+    }, ms)
+  }
+  return wrapper;
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+
+//task 3
+
+function debounceDecorator2(debounceDecoratorNew) {
+  let count = 0;
+
+  function wrapper(...rest) {
+    wrapper.history = count++;
+    return debounceDecoratorNew.call(this, ...rest);
+  }
+  return wrapper;
 }
